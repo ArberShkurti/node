1c521e85e8cc08e7f54370deb8c600f5fe6d5ad1
Arber Shkurti <arbershkurti@yahoo.com>
2024-02-06 21:54:43 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+// const numero = 3;
+
+// if ( numero < 3) {
+//     console.log("me i vogel");
+// } else if ( numero == 3 ) {
+//     console.log("baraz");
+// } else {
+//     console.log ("me i madh"); 
+// }
+
+for (i=0; i<10; i++){
+ console.log((i));
+}
