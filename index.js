e9c307562f8c550a6d500430f9dec13b50856a3e
Arber Shkurti <arbershkurti@yahoo.com>
2024-02-11 21:46:07 +0100

    onemit

--- index.js
+++ index.js
@@ -19,23 +19,40 @@
 // const sayHi = require('./utilities')
 // const name = require('./name')
 
-// console.log(name)
+// console.log(name);
 
 
 // sayHi(name1);
 // sayHi("anna");
 // sayHi(name2);
+// const httpja = require ('http');
 
-const httpja = require ('http');
+// const server = httpja.createServer((request, response)=>{
+//    if (request.url === "/") {
+//     response.end("welcome again");
+//    } else {
+//     response.end(`<p> back to <a href="/">home</a>`);
+//    }
+// })
 
-const server = httpja.createServer((request, response)=>{
-   if (request.url === "/") {
-    response.end("welcome again");
-   } else {
-    response.end(`<p> back to <a href="/">home</a>`);
-   }
-})
-
-server.listen(3000);
+// server.listen(3000);
 
 // console.log(httpja());
+
+
+// const logu = 55;
+
+// console.log(logu);
+
+
+// const EventEmitter = require ('events');
+// const { loadavg } = require('os');
+// const { log } = require('util');
+
+const customEmitter = new EventEmitter();
+
+customEmitter.on('sms', ()=>{
+    console.log('started');
+})
+
+customEmitter.emit('sms');
--- utilities.js
+++ utilities.js
@@ -4,4 +4,4 @@ function sayHi(name) {
 }
 
 
-module.exports = sayHi
+module.exports = sayHi;
