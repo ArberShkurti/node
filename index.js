0fc96b935489545126e1bb133e889fff16fb1f0d
Arber Shkurti <arbershkurti@yahoo.com>
2024-02-21 22:53:50 +0100

    api2

--- index.js
+++ index.js
@@ -153,7 +153,16 @@ const app = express()
 const {persone} = require('./persone')
 
 app.get('/api/persone', (req,res)=>{
-    res.status(200).json({data : persone})
+    res.status(200).json({success: true, data : persone})
+})
+
+app.get('/api/persone/:id', (req,res)=>{
+    const {id} = req.params
+
+    const persona = persone.find(
+        (persona) => persona.id === id
+    )
+    res.json({success: true, data: persona})
 })
 
 app.listen(5000)
