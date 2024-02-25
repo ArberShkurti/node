a46665155664243c2096cdd99ef6dc227307748f
Arber Shkurti <arbershkurti@yahoo.com>
2024-02-25 19:22:44 +0100

    router

--- index.js
+++ index.js
@@ -150,43 +150,13 @@
 
 const express = require('express')
 const app = express()
-const {persone} = require('./persone')
+const personeRouter = require('./routes/persone')
+
 
 app.use(express.json())
 
-app.get('/api/persone', (req,res)=>{
-    res.status(200).json({success: true, data : persone})
-})
+app.use('/api/persone', personeRouter)
 
-app.get('/api/persone/:id', (req,res)=>{
-    const {id} = req.params
 
-    const persona = persone.find(
-        (persona) => persona.id === id
-    )
-    res.json({success: true, data: persona})
-})
-
-app.post ('/api/persone', (req, res)=>{
-    console.log(req.body);
-    const persona = req.body
-    persone.push(persona)
-    res.status(200).json({success: true, data : persone})
-})
-
-app.put('/api/persone/:id', (req, res)=>{
-    const {id} = req.params
-    const persona = req.body
-    persone[Number(id) - 1] = persona
-    res.status(200).json({success: true, data : persone})
-})
-
-app.delete('/api/persone/:id', (req, res) => {
-    const {id} = req.params
-    const index = persone.findIndex(persona => persona.id === id)
-    persone.splice(index,1)
-    res.status(200).json({success: true, data:persone})
-
-})
 
 app.listen(5000)
